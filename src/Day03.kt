6f5ea485cb9b37fdbeb47719868badd7d550195a
michaelenoma <enomamichael@gmail.com>
2023-01-16 18:57:40 +0100

    Convert argument to test

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,15 +7,15 @@ fun main() {
     fun part1(input: List<String>): Int {
 
         val items = input.map { s -> Pair(s.substring(0, s.length / 2), s.substring(s.length / 2)) }
-        val commonItems = items.map { it.first.asIterable().intersect(it.second.asIterable()).first() }
+        val commonItems = items.map { it.first.asIterable().intersect(it.second.asIterable().toSet()).first() }
         val priorities = commonItems.map { toPriority(it) }
         return priorities.sum()
     }
 
     fun part2(input: List<String>): Int {
         val items = input.chunked(3)
-        val commonItems = items.map { it[0].asIterable().intersect(it[1].asIterable()).toList() }
-        val commonItems2 = items.mapIndexed { idx, l -> l[2].asIterable().intersect(commonItems[idx]).first() }
+        val commonItems = items.map { it[0].asIterable().intersect(it[1].asIterable().toSet()).toList() }
+        val commonItems2 = items.mapIndexed { idx, l -> l[2].asIterable().intersect(commonItems[idx].toSet()).first() }
 
         val priorities = commonItems2.map { toPriority(it) }
 
