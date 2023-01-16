7d51791b64e1931c2e02c5e399667d7c20bcb53b
michaelenoma <enomamichael@gmail.com>
2023-01-16 18:46:07 +0100

    Day 2 part1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,62 @@
-package day2
+fun main() {
 
-class Day02 {
+    fun getHandFromLine(line: String): Int {
+        return if (line.contains("X")) {
+            1
+        } else if (line.contains("Y")) {
+            2
+        } else {
+            3
+        }
+    }
+
+    fun getGameFromLine(line: String): Int {
+        return if (line == "A X") {
+            3
+        } else if (line.contains("A Y")) {
+            6
+        } else if (line.contains("A Z")) {
+            0
+        } else if (line.contains("B X")) {
+            0
+        } else if (line.contains("B Y")) {
+            3
+        } else if (line.contains("B Z")) {
+            6
+        } else if (line.contains("C X")) {
+            6
+        } else if (line.contains("C Y")) {
+            0
+        } else {
+            3
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+
+        // Could do this with a fold, but will do in two steps for step 2
+        val shapes = input.map { getHandFromLine(it) }
+        val games = input.map { getGameFromLine(it) }
+
+        val scores = shapes.zip(games) { xv, yv -> xv + yv }
+
+        return scores.sum()
+
+
+        // test if implementation meets criteria from the description, like:
+        val testInput = readInput("Day02_test")
+        check(part1(testInput) == 15)
+
+        val input = readInput("Day02")
+        println(part1(input))
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+
+    val input = readInput("Day02")
+    println(part1(input))
 }
 
 
