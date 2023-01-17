54acde800032fd69bbf637aeee6ece73504f4c7e
michaelenoma <enomamichael@gmail.com>
2023-01-17 23:38:33 +0100

    Day 4- Part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,28 +1,43 @@
-
-
 fun main() {
     fun part1(input: List<String>): Int {
         var contained = 0
-        for (line in input){
+        for (line in input) {
             val elves = line.split(",")
             val rangesAsList = elves.map { it.split("-") }
-            val ranges = rangesAsList.map { (it[0].toInt()..it[1].toInt()).toList()   }
+            val ranges = rangesAsList.map { (it[0].toInt()..it[1].toInt()).toList() }
 
-            if(ranges[0].containsAll(ranges[1]) || ranges[1].containsAll(ranges[0])){
+            if (ranges[0].containsAll(ranges[1]) || ranges[1].containsAll(ranges[0])) {
                 contained += 1
             }
         }
 
         return contained
     }
+
+    fun part2(input: List<String>): Int {
+        var overlap = 0
+        for (line in input) {
+            val elves = line.split(",")
+            val rangesAsList = elves.map { it.split("-") }
+            val ranges = rangesAsList.map { (it[0].toInt()..it[1].toInt()).toList() }
+
+            if (ranges[0].any { ranges[1].contains(it) } || ranges[1].any { ranges[0].contains(it) }) {
+                overlap += 1
+            }
+        }
+
+        return overlap
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
+    println(part2(input))
 }
-
 
 /** Part1
  * --- Day 4: Camp Cleanup ---
@@ -65,4 +80,19 @@ fun main() {
  * Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
  *
  * In how many assignment pairs does one range fully contain the other?
+ */
+
+/**Part 2
+ * --- Part Two ---
+ * It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+ *
+ * In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+ *
+ * 5-7,7-9 overlaps in a single section, 7.
+ * 2-8,3-7 overlaps all of the sections 3 through 7.
+ * 6-6,4-6 overlaps in a single section, 6.
+ * 2-6,4-8 overlaps in sections 4, 5, and 6.
+ * So, in this example, the number of overlapping assignment pairs is 4.
+ *
+ * In how many assignment pairs do the ranges overlap?
  */
